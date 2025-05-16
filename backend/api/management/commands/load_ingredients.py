3ea563e09a3e89524494b13b9964edc3d4d70b98
PaiNaiP <77003061+PaiNaiP@users.noreply.github.com>
2025-05-16 20:26:45 +0300

    Update pep8

--- backend/api/management/commands/load_ingredients.py
+++ backend/api/management/commands/load_ingredients.py
@@ -1,7 +1,7 @@
 import json
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from ingredients.models import Ingredient  
+from ingredients.models import Ingredient
 
 
 class Command(BaseCommand):
